734da168f8710b7bbd8317ad3cb7d771ee79bb80
ReltTull <92668493+ReltTull@users.noreply.github.com>
2022-06-28 14:36:31 +0300

    a little bit of formating

--- MergeSort.java
+++ MergeSort.java
@@ -1,5 +1,5 @@
 public class MergeSort {
-    public static void dividingAndMerging (int[] arr) {
+    public static void dividingAndMerging(int[] arr) {
         if (arr.length < 2) {
             return;
         }
@@ -7,10 +7,10 @@ public class MergeSort {
         int[] leftPart = new int[mid];
         int[] rightPart = new int[arr.length - mid];
 
-        for(int i = 0; i < mid; i++) {
+        for (int i = 0; i < mid; i++) {
             leftPart[i] = arr[i];
         }
-        for(int i = mid; i < arr.length; i++) {
+        for (int i = mid; i < arr.length; i++) {
             // Влад, привет, если прочитаешь, то подскажи в ответе пожалуйста, почему в индекс задаётся
             // "i - mid",а не i? Я это решение нашёл, разобрал и переписал по памяти, но тут индекс вылетал
             // из rightPart.lenght, и пришлось подсматривать снова. Единственный момент, который не понял в коде.
@@ -25,14 +25,14 @@ public class MergeSort {
         merge(arr, leftPart, rightPart, mid, arr.length - mid);
     }
 
-    public static void merge (int[] unitArr, int[] leftArr, int[] rightArr, int leftLenght, int rightLenght) {
+    public static void merge(int[] unitArr, int[] leftArr, int[] rightArr, int leftLenght, int rightLenght) {
         int leftCounter = 0;
         int rightCounter = 0;
         int unitedCounter = 0;
 
         // сравниваем поэлементно массивы и меньший из пары добавляем в arr.
         while (leftCounter < leftLenght && rightCounter < rightLenght) {
-            if(leftArr[leftCounter] <= rightArr[rightCounter]) {
+            if (leftArr[leftCounter] <= rightArr[rightCounter]) {
                 unitArr[unitedCounter++] = leftArr[leftCounter++];
             } else {
                 unitArr[unitedCounter++] = rightArr[rightCounter++];
@@ -48,7 +48,7 @@ public class MergeSort {
     }
 
     public static void main(String[] args) {
-        int[] actual = { 5, 1, 6, 2, 3, 4, 4, 10, 27, 0, 5};
+        int[] actual = {5, 1, 6, 2, 3, 4, 4, 10, 27, 0, 5};
         dividingAndMerging(actual);
         for (int i = 0; i < actual.length; i++) {
             System.out.print(actual[i] + " ");
